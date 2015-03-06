3acd581bba1269e421d3a8c7804f2d5af2ce1f6d
Shaun Jackman <sjackman@gmail.com>
2015-03-06 10:53:59 -0800

    gcc48 4.8.4: Fix for Linuxbrew

--- gcc48.rb
+++ gcc48.rb
@@ -39,18 +39,24 @@ class Gcc48 < Formula
   option 'enable-nls', 'Build with native language support (localization)'
   option 'enable-profiled-build', 'Make use of profile guided optimization when bootstrapping GCC'
   # enabling multilib on a host that can't run 64-bit results in build failures
-  option 'disable-multilib', 'Build without multilib support' if MacOS.prefer_64_bit?
+  if OS.mac?
+    option "without-multilib", "Build without multilib support" if MacOS.prefer_64_bit?
+  else
+    option "with-multilib", "Build with multilib support"
+  end
 
-  depends_on 'gmp4'
-  depends_on 'libmpc08'
-  depends_on 'mpfr2'
-  depends_on 'cloog018'
-  depends_on 'isl011'
+  depends_on "binutils" if build.with? "glibc"
+  depends_on "glibc" => :optional
+  depends_on 'gmp'
+  depends_on 'libmpc'
+  depends_on 'mpfr'
+  depends_on 'cloog'
+  depends_on 'isl'
   depends_on 'ecj' if build.include? 'enable-java' or build.include? 'enable-all-languages'
 
   # The as that comes with Tiger isn't capable of dealing with the
   # PPC asm that comes in libitm
-  depends_on 'cctools' => :build if MacOS.version < :leopard
+  depends_on 'cctools' => :build if OS.mac? && MacOS.version < :leopard
 
   fails_with :gcc_4_0
   fails_with :llvm
@@ -58,11 +64,15 @@ class Gcc48 < Formula
   # GCC bootstraps itself, so it is OK to have an incompatible C++ stdlib
   cxxstdlib_check :skip
 
+  def version_suffix
+    version.to_s.slice(/\d\.\d/)
+  end
+
   def install
     # GCC will suffer build errors if forced to use a particular linker.
     ENV.delete 'LD'
 
-    if MacOS.version < :leopard
+    if OS.mac? && MacOS.version < :leopard
       ENV["AS"] = ENV["AS_FOR_TARGET"] = "#{Formula["cctools"].bin}/as"
     end
 
@@ -79,20 +89,18 @@ class Gcc48 < Formula
       languages << 'java' if build.include? 'enable-java'
     end
 
-    version_suffix = version.to_s.slice(/\d\.\d/)
-
     args = [
-      "--build=#{arch}-apple-darwin#{osmajor}",
+      ("--build=#{arch}-apple-darwin#{osmajor}" if OS.mac?),
       "--prefix=#{prefix}",
-      "--libdir=#{lib}/gcc/#{version_suffix}",
+      ("--libdir=#{lib}/gcc/#{version_suffix}" if OS.mac?),
       "--enable-languages=#{languages.join(',')}",
       # Make most executables versioned to avoid conflicts.
       "--program-suffix=-#{version_suffix}",
-      "--with-gmp=#{Formula["gmp4"].opt_prefix}",
-      "--with-mpfr=#{Formula["mpfr2"].opt_prefix}",
-      "--with-mpc=#{Formula["libmpc08"].opt_prefix}",
-      "--with-cloog=#{Formula["cloog018"].opt_prefix}",
-      "--with-isl=#{Formula["isl011"].opt_prefix}",
+      "--with-gmp=#{Formula["gmp"].opt_prefix}",
+      "--with-mpfr=#{Formula["mpfr"].opt_prefix}",
+      "--with-mpc=#{Formula["libmpc"].opt_prefix}",
+      "--with-cloog=#{Formula["cloog"].opt_prefix}",
+      "--with-isl=#{Formula["isl"].opt_prefix}",
       "--with-system-zlib",
       "--enable-libstdcxx-time=yes",
       "--enable-stage1-checking",
@@ -104,14 +112,22 @@ class Gcc48 < Formula
       "--with-pkgversion=Homebrew #{name} #{pkg_version} #{build.used_options*" "}".strip,
       "--with-bugurl=https://github.com/Homebrew/homebrew-versions/issues",
     ]
+    if build.with? "glibc"
+      binutils = Formula["binutils"].prefix/"x86_64-unknown-linux-gnu/bin"
+      args += [
+        "--with-native-system-header-dir=#{HOMEBREW_PREFIX}/include",
+        "--with-build-time-tools=#{binutils}",
+        "--with-boot-ldflags=-static-libstdc++ -static-libgcc #{ENV["LDFLAGS"]}",
+      ]
+    end
 
     # "Building GCC with plugin support requires a host that supports
     # -fPIC, -shared, -ldl and -rdynamic."
-    args << "--enable-plugin" if MacOS.version > :tiger
+    args << "--enable-plugin" if !OS.mac? || MacOS.version > :tiger
 
     # Otherwise make fails during comparison at stage 3
     # See: http://gcc.gnu.org/bugzilla/show_bug.cgi?id=45248
-    args << '--with-dwarf2' if MacOS.version < :leopard
+    args << '--with-dwarf2' if OS.mac? || MacOS.version < :leopard
 
     args << '--disable-nls' unless build.include? 'enable-nls'
 
@@ -119,10 +135,10 @@ class Gcc48 < Formula
       args << "--with-ecj-jar=#{Formula["ecj"].opt_prefix}/share/java/ecj.jar"
     end
 
-    if !MacOS.prefer_64_bit? || build.include?('disable-multilib')
-      args << '--disable-multilib'
+    if build.without?("multilib") || !MacOS.prefer_64_bit?
+      args << "--disable-multilib"
     else
-      args << '--enable-multilib'
+      args << "--enable-multilib"
     end
 
     # Ensure correct install names when linking against libgcc_s;
@@ -130,7 +146,7 @@ class Gcc48 < Formula
     inreplace "libgcc/config/t-slibgcc-darwin", "@shlib_slibdir@", "#{HOMEBREW_PREFIX}/lib/gcc/#{version_suffix}"
 
     mkdir 'build' do
-      unless MacOS::CLT.installed?
+      if OS.mac? && !MacOS::CLT.installed?
         # For Xcode-only systems, we need to tell the sysroot path.
         # 'native-system-header's will be appended
         args << "--with-native-system-header-dir=/usr/include"
@@ -179,6 +195,42 @@ class Gcc48 < Formula
       config_files.each do |file|
         add_suffix file, version_suffix if File.exist? file
       end
+    end
+  end
+
+  def post_install
+    if OS.linux?
+      # Create the GCC specs file
+      # See https://gcc.gnu.org/onlinedocs/gcc/Spec-Files.html
+
+      # Locate the specs file
+      gcc = "gcc-#{version_suffix}"
+      specs = Pathname.new(`#{bin}/#{gcc} -print-libgcc-file-name`).dirname/"specs"
+      ohai "Creating the GCC specs file: #{specs}"
+      raise "command failed: #{gcc} -print-libgcc-file-name" if $?.exitstatus != 0
+      specs_orig = Pathname.new("#{specs}.orig")
+      rm_f [specs_orig, specs]
+
+      # Save a backup of the default specs file
+      s = `#{bin}/#{gcc} -dumpspecs`
+      raise "command failed: #{gcc} -dumpspecs" if $?.exitstatus != 0
+      specs_orig.write s
+
+      # Set the library search path
+      if build.with?("glibc")
+        s += "*link_libgcc:\n-nostdlib -L#{lib}/gcc/x86_64-unknown-linux-gnu/#{version} -L#{HOMEBREW_PREFIX}/lib\n\n"
+      else
+        s += "*link_libgcc:\n+ -L#{HOMEBREW_PREFIX}/lib\n\n"
+      end
+      s += "*link:\n+ -rpath #{HOMEBREW_PREFIX}/lib"
+
+      # Set the dynamic linker
+      glibc = Formula["glibc"]
+      if glibc.installed?
+        s += " --dynamic-linker #{glibc.opt_lib}/ld-linux-x86-64.so.2"
+      end
+      s += "\n\n"
+      specs.write s
     end
   end
 
